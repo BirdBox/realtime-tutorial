300e9cbe83ca96e7b207bffc6a61a105d0e34e12
Tres Spicher <tres@tspike.com>
2013-07-25 14:26:30 -0600

    minor tweaks to js

--- js-client/warble.js
+++ js-client/warble.js
@@ -1,31 +1,34 @@
 (function() {
   // Initialize the socket & handlers
-  var warbleSocket,
-      connectToServer = function() {
-        warbleSocket = new SockJS('http://localhost:5555/warble');
-        warbleSocket.onopen = function() {
-          clearInterval(connectRetry);
-          $('.connect-status')
-            .removeClass('disconnected')
-            .addClass('connected')
-            .text('Connected');
-        };
-        warbleSocket.onmessage = function(e) {
-          $('#warble-msg').text(e.data);
-        };
-        warbleSocket.onclose = function() {
-          clearInterval(connectRetry);
-          connectRetry = setInterval(connectToServer, 1000);
-          $('.connect-status')
-            .removeClass('connected')
-            .addClass('disconnected')
-            .text('Disconnected');
-        };
+  var connectToServer = function() {
+    var warbleSocket = new SockJS('http://localhost:5555/warble');
 
-        // Connect the text field to the socket
-        $('.msg-sender').off('input').on('input', function() {
-          warbleSocket.send($('.msg-sender input').val()); 
-        });
-      },
+    warbleSocket.onopen = function() {
+      clearInterval(connectRetry);
+      $('.connect-status')
+        .removeClass('disconnected')
+        .addClass('connected')
+        .text('Connected');
+    };
+
+    warbleSocket.onmessage = function(e) {
+      $('#warble-msg').text(e.data);
+    };
+
+    warbleSocket.onclose = function() {
+      clearInterval(connectRetry);
       connectRetry = setInterval(connectToServer, 1000);
+      $('.connect-status')
+        .removeClass('connected')
+        .addClass('disconnected')
+        .text('Disconnected');
+    };
+
+    // Connect the text field to the socket
+    $('.msg-sender').off('input').on('input', function() {
+      warbleSocket.send($('.msg-sender input').val()); 
+    });
+  };
+
+  var connectRetry = setInterval(connectToServer, 1000);
 })();
